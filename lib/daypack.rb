7c21f829c99b44b22590622f44c5fed60ca28df6
Ray Bradley <rgb@fastmail.com>
2023-04-21 08:35:54 -0400

    Initial commit

---
+++ lib/daypack.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "daypack/version"
+
+module Daypack
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/daypack_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Daypack do
+  it "has a version number" do
+    expect(Daypack::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
